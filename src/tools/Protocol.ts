7a0b16c609423c66dfe48914bb3b0efa2b0b16f4
equationslayer12 <roy0708usa@gmail.com>
2024-03-18 13:22:13 +0200

    started syncing data in game

---
+++ src/tools/Protocol.ts
@@ -0,0 +1,8 @@
+export default class Protocol {
+    static Encrypt = class {
+        static changeLetter(originLetter: string, gussedLetter: string) {
+            return `CL;${originLetter};${gussedLetter}`
+    }
+
+    }
+}
--- src/views/PracticeView.vue
+++ src/views/PracticeView.vue
@@ -1,7 +1,7 @@
 <template>
     <h1 class="text-4xl font-semibold my-14 hover:scale-125 duration-150 text-accent-color">Span Racer</h1>
     <span v-if="text">
-        <CipherAnalysisComponent :text="text"/>
+        <CipherAnalysisComponent :text="text" @letterChange="sendChangedLetter"/>
     </span>
     <span v-else>
         loading...
@@ -10,12 +10,13 @@
 
 <script setup lang="ts">
     import CipherAnalysisComponent from '../components/CipherAnalysisComponent.vue';
+    import Protocol from '../tools/Protocol'
     import axios from 'axios'
     import {ref} from 'vue'
     
     // const text = "DJ DK C QLXDWI WF SDGDU PCX. XLRLU KQCSLKBDQK, KJXDHDET FXWZ C BDIILE RCKL, BCGL PWE JBLDX FDXKJ GDSJWXO CTCDEKJ JBL LGDU TCUCSJDS LZQDXL. IYXDET JBL RCJJUL, XLRLU KQDLK ZCECTLI JW KJLCU KLSXLJ QUCEK JW JBL LZQDXL’K YUJDZCJL PLCQWE, JBL ILCJB KJCX, CE CXZWXLI KQCSL KJCJDWE PDJB LEWYTB QWPLX JW ILKJXWO CE LEJDXL QUCELJ. QYXKYLI RO JBL LZQDXL’K KDEDKJLX CTLEJK, QXDESLKK ULDC XCSLK BWZL CRWCXI BLX KJCXKBDQ, SYKJWIDCE WF JBL KJWULE QUCEK JBCJ SCE KCGL BLX QLWQUL CEI XLKJWXL FXLLIWZ JW JBL TCUCVO…";
     const text = ref('');
-    // var ws: WebSocket | null = null;
+    var socket: WebSocket | null = null;
 
     init();
     async function init() {
@@ -32,16 +33,16 @@
             return null;
     }
     async function connectToServerSocket() {
+        console.log("start connecting")
         var ws = new WebSocket('ws://localhost:8080/api/practice')
         ws.onopen = (event) => {
-            console.log("sending heyyyy");
-            ws.send("heyyyy");
+            console.log("Connected to server socket");
+            socket = ws;
         };
     }
-    async function startSendingData(event) {
-        console.log("starting to send data :)");
-        console.log(ws);
-        ws.send("heyyyy");
-        // ws.close()
+    async function sendChangedLetter(originLetter: string, gussedLetter: string) {
+        const message = Protocol.Encrypt.changeLetter(originLetter, gussedLetter);
+        console.log(message);
+        socket?.send(message);
     }
 </script>
